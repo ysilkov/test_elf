3299e7dde7a05481f44528ee683cdfcab4df9ca1
ysilkov <ysilkov@ukr.net>
2023-05-29 11:06:22 +0300

    create directionsService

--- client/app/api/google.tsx
+++ client/app/api/google.tsx
@@ -8,8 +8,8 @@ const containerStyle = {
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 49.58826699999999,
+  lng: 34.551417,
 };
 
 interface Props {
@@ -25,6 +25,10 @@ export default function MyComponent(props: Props) {
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [customerLocation, setCustomerLocation] =
     useState<google.maps.LatLngLiteral | null>(null);
+  const [directionsService, setDirectionsService] =
+    useState<google.maps.DirectionsService | null>(null);
+  const [directionsRenderer, setDirectionsRenderer] =
+    useState<google.maps.DirectionsRenderer | null>(null);
 
   useEffect(() => {
     if (props.address) {
@@ -34,19 +38,44 @@ export default function MyComponent(props: Props) {
           const { lat, lng } = results[0].geometry.location;
           setCustomerLocation({ lat: lat(), lng: lng() });
           map?.setCenter({ lat: lat(), lng: lng() });
+          calculateRoute({ lat: lat(), lng: lng() });
         }
       });
     }
   }, [props.address, map]);
+
   const onLoad = React.useCallback(function callback(map: google.maps.Map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
     setMap(map);
+    setDirectionsService(new window.google.maps.DirectionsService());
+    setDirectionsRenderer(new window.google.maps.DirectionsRenderer());
   }, []);
 
   const onUnmount = React.useCallback(function callback() {
     setMap(null);
   }, []);
+
+  const calculateRoute = (destination: google.maps.LatLngLiteral) => {
+    if (directionsService && directionsRenderer && customerLocation) {
+      const origin = new window.google.maps.LatLng(center.lat, center.lng);
+      const request = {
+        origin: origin,
+        destination: new window.google.maps.LatLng(
+          destination.lat,
+          destination.lng
+        ),
+        travelMode: google.maps.TravelMode.DRIVING,
+      };
+      directionsService.route(request, (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsRenderer.setDirections(result);
+        }
+      });
+
+      directionsRenderer.setMap(map);
+    }
+  };
 
   return isLoaded ? (
     <div>
